b724a1471da6478cdf432ff37bd3d865b0c68ae6
yukaina <misaki.yukaina@gmail.com>
2016-02-07 22:22:10 +0900

    master add .simplecov

---
+++ .simplecov
@@ -0,0 +1,7 @@
+SimpleCov.start do
+  add_filter '/spec/html/'
+  formatter SimpleCov::Formatter::MultiFormatter[
+              SimpleCov::Formatter::HTMLFormatter,
+              CodeClimate::TestReporter::Formatter
+            ]
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,3 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'pdql'
 require 'codeclimate-test-reporter'
-SimpleCov.start do
-  formatter SimpleCov::Formatter::MultiFormatter[
-                SimpleCov::Formatter::HTMLFormatter,
-                CodeClimate::TestReporter::Formatter
-            ]
-end
